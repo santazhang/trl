5cb1c7f50b364ebba07835bd56f1073d2cc970c0
Santa Zhang <santa1987@gmail.com>
2014-05-05 16:50:39 -0400

    getting ready for demo

--- webdemo/launch-clients.sh
+++ webdemo/launch-clients.sh
@@ -2,6 +2,6 @@
 
 source conf/env.sh
 
-for client_id in $(seq $1 $2 $3); do
-  RLOG_SERVER=127.0.0.1:8848 ../bin/trl_client $client_id &
+for client_id in $(seq 1 $1 50); do
+  RLOG_SERVER=noi.yzhang.net:8848 ../bin/trl_client $client_id &
 done
